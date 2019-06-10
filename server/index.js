b3201a7e51ca77d961c5a5471b86a5210076469d
Cezar Mocan <mocancezar@gmail.com>
2019-06-10 17:41:16 -0400

    Start server in centralized phase

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ if (Object.keys(GARDENS).indexOf(process.argv[2].toLowerCase()) == -1) {
 }
 
 setGarden(process.argv[2])
-// setPerformancePhase(PERFORMANCE_PHASES.DISTRIBUTED)
+setPerformancePhase(PERFORMANCE_PHASES.CENTRALIZED)
 // DistributedManager.helloCreature("creature1")
 
 const GARDEN_CONFIG = getGardenConfig()
